3b0ff693bb5537e5f68e8862bdfaa68e4f36a411
tjoeun <tjoeun@host.docker.internal>
2024-03-21 09:33:48 +0900

    240320

---
+++ FirstJava/src/D/D07_checkedExceptionEx.java
@@ -0,0 +1,20 @@
+package D;
+import java.io.IOException;
+
+public class D07_checkedExceptionEx {
+
+	public static void main(String[] args) {
+		// checkedException은 예외처리 해야 함. 잘못된 값이 들어와서 예외발생 가능...
+		System.out.println("값을 입력받는 프로그램입니다.");
+		
+		byte[] data = new byte[100];	// 한번에 100byte 처리할 공간을 의미함.
+		try {
+			System.in.read(data);
+		}catch (IOException e) {	//입출력과 관련된 Exception
+			e.printStackTrace();
+		}
+		System.out.print("입력할 문자열은 : ");
+		System.out.println(new String(data).trim());
+	}
+	
+}
---
+++ FirstJava/src/D/D07_runtimeException.java
@@ -0,0 +1,63 @@
+package D;
+
+public class D07_runtimeException {
+	public static void main(String[] args) throws Exception {
+		// Runtime Exception = UncheckedException...
+//		makeException(5);
+//		makeException2();
+		try {makeException3();
+			makeException4();
+			makeException5();
+//			makeException6();
+			makeException7();
+		}
+		catch (Exception e) {
+			System.out.println("예외발생");
+		}
+		makeException8();
+	}
+
+	public static int makeException(int input) {
+		int divide = 0;
+		int result = input/divide;
+		return result;
+	}
+	
+	public static void makeException2() {
+		String s = "가";
+		System.out.println(s.charAt(1));
+	}
+	
+	public static void makeException3() throws Exception {
+		int[] a = {1,2,3};
+		System.out.println(a[3]);
+	}
+	
+	public static void makeException4() {
+		String nothing = null;
+		System.out.println(nothing.split(""));
+	}
+	
+	public static void makeException5() {
+		String notInt = "가나다";
+		int a = Integer.parseInt(notInt);
+	}
+	
+	public static void makeException6() {
+		for (int i=0; i < Integer.MAX_VALUE; i++) {	//MAX_VALUE = 
+			makeException6();
+		}
+	}
+	
+	public static void makeException7() {	//ArrayStoreException : 배열저장예외.
+		Object[] objArr = new String[3];
+		objArr[0] = 0;	//선언 자체가 Object로 되어있으므로 컴파일 오류 발생 X
+	}
+	
+	public static void makeException8() {	//ClassCastException
+		Object[] objArr = new String[3];
+		objArr[0] = "0";
+		Integer a = (Integer) objArr[0];	//[]안에 0은 정수가 아님.
+	}
+
+}
