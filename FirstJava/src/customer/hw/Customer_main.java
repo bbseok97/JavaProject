6ed090d880b70b9569473cf135d8bb3e864d2bde
tjoeun <tjoeun@DESKTOP-PLUDL37.mshome.net>
2024-04-04 18:18:10 +0900

    240404 숙제

--- FirstJava/src/customer/hw/Customer_main.java
+++ FirstJava/src/customer/hw/Customer_main.java
@@ -17,8 +17,8 @@ public class Customer_main {
 	         System.out.println(" 1. 고객 정보 추가");
 	         System.out.println(" 2. 고객 정보 수정");
 	         System.out.println(" 3. 고객 정보 삭제");
-	         System.out.println(" 4. 고객 정보 전체 조회");
-	         System.out.println(" 5. 특정 고객 조회");
+	         System.out.println(" 4. 이름으로 고객 조회");
+	         System.out.println(" 5. 전체 고객 조회");
 	         System.out.println(" 6. 프로그램 종료");
 	         System.out.println(" 메뉴를 선택하세요 ");
 	         
@@ -60,6 +60,14 @@ public class Customer_main {
 	            }else {
 	            	System.out.println("일치하는 고객명이 없습니다.");
 	            }
+	            break;
+	            
+	         case 3:
+	        	 System.out.print("삭제할 고객님의 이름을 입력하세요: ");
+	        	 String deleteName = scan.nextLine();
+	        	 Customer deleteInfo = manager.searchInfo(deleteName);
+	        	 
+	        	 
 	         }
 	         
 	         
